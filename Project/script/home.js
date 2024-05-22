6c03074f33925ad7d3c3c76d4ee4a3c797154004
Mohamed Nezhari <109751834+Nezgova@users.noreply.github.com>
2024-05-22 09:36:47 +0100

    update main page
    ..

--- Project/script/home.js
+++ Project/script/home.js
@@ -1,6 +1,7 @@
 const menuIcon = document.querySelector('#menu-icon');
 const navbar = document.querySelector('.navbar');
 const navbg = document.querySelector('.nav-bg');
+
 menuIcon.addEventListener('click', () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
