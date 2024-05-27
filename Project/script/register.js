38321fcac9d386d335804172b59d8f7cf8ea754b
Mohamed Nezhari <109751834+Nezgova@users.noreply.github.com>
2024-05-27 09:20:12 +0100

    update

--- Project/script/register.js
+++ Project/script/register.js
@@ -1,29 +1,39 @@
 /*=============== SHOW HIDDEN - PASSWORD ===============*/
-const showHiddenPass = (loginPass, loginEye) => {
+const showHiddenPass = (loginPass, loginEye, confirmPass, confirmEye) => {
     const input = document.getElementById(loginPass),
           iconEye = document.getElementById(loginEye);
+    const confirmInput = document.getElementById(confirmPass),
+          confirmIconEye = document.getElementById(confirmEye);
 
     iconEye.addEventListener('click', () => {
         // Change password to text
         if (input.type === 'password') {
-            // Switch to text
             input.type = 'text';
-
-            // Icon change
             iconEye.classList.add('ri-eye-line');
             iconEye.classList.remove('ri-eye-off-line');
         } else {
-            // Change to password
             input.type = 'password';
-
-            // Icon change
             iconEye.classList.remove('ri-eye-line');
             iconEye.classList.add('ri-eye-off-line');
         }
     });
+
+    confirmIconEye.addEventListener('click', () => {
+        // Change confirm password to text
+        if (confirmInput.type === 'password') {
+            confirmInput.type = 'text';
+            confirmIconEye.classList.add('ri-eye-line');
+            confirmIconEye.classList.remove('ri-eye-off-line');
+        } else {
+            confirmInput.type = 'password';
+            confirmIconEye.classList.remove('ri-eye-line');
+            confirmIconEye.classList.add('ri-eye-off-line');
+        }
+    });
 };
 
-showHiddenPass('login-pass', 'login-eye');
+// Initialize the showHiddenPass function with IDs
+showHiddenPass('login-pass', 'login-eye', 'confirm-pass', 'confirm-eye');
 
 /*=============== FORM SUBMISSION ===============*/
 const form = document.getElementById('login-form');
